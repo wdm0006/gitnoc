271e63c23774326369693aa7eb615158c6920239
Will McGinnis <will@pedalwrencher.com>
2015-12-07 21:55:03 -0500

    risk table added

--- gitnoc/app.py
+++ gitnoc/app.py
@@ -11,17 +11,24 @@ scripts = [
     "./bower_components/jquery/dist/jquery.min.js",
     "./bower_components/d3/d3.min.js",
     "./bower_components/nvd3/build/nv.d3.min.js",
-    "./js/main.js",
+    "./bower_components/datatables/media/js/jquery.dataTables.min.js",
+    "./bower_components/datatables-buttons/js/dataTables.buttons.js"
 ]
 
 css = [
     "./bower_components/bootstrap/dist/css/bootstrap.min.css",
     "./bower_components/nvd3/build/nv.d3.min.css",
+    "./bower_components/datatables/media/css/jquery.dataTables.min.css",
+    "./bower_components/datatables-buttons/css/buttons.dataTables.scss",
     "./css/main.css"
 ]
 
 app = Flask(__name__)
 app.secret_key = 'CHANGEME'
+
+directory = str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data'
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 
 def get_settings():
@@ -48,7 +55,7 @@ def settings():
             'extensions': form.extensions.data
         })
         bp = str(os.path.dirname(os.path.abspath(__file__)))
-        json.dump(settings, open(bp + os.sep + 'settings.json', 'w'))
+        json.dump(settings, open(bp + os.sep + 'settings.json', 'w'), indent=4)
 
     settings = get_settings()
     extensions = settings.get('extensions', None)
@@ -71,7 +78,7 @@ def settings():
     return render_template('settings.html', form=form, title=TITLE, base_scripts=scripts, css=css)
 
 
-@app.route('/cumulative_author_blame', methods=['GET', 'POST'])
+@app.route('/cumulative_author_blame', methods=['GET'])
 def cumulative_author_blame():
     settings = get_settings()
     project_dir = settings.get('project_dir', os.getcwd())
@@ -95,11 +102,11 @@ def cumulative_author_blame():
 
     # dump the data to disk
     filename = 'cumulative_author_blame.json'
-    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'))
+    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'), indent=4)
     return redirect(url_for('index'))
 
 
-@app.route('/cumulative_project_blame', methods=['GET', 'POST'])
+@app.route('/cumulative_project_blame', methods=['GET'])
 def cumulative_project_blame():
     settings = get_settings()
     project_dir = settings.get('project_dir', os.getcwd())
@@ -123,8 +130,33 @@ def cumulative_project_blame():
 
     # dump the data to disk
     filename = 'cumulative_project_blame.json'
-    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'))
+    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'), indent=4)
     return redirect(url_for('index'))
+
+
+@app.route('/risk', methods=['GET', 'POST'])
+def risk():
+    return render_template('filechangerates.html', title=TITLE, base_scripts=scripts, css=css)
+
+
+@app.route('/file_change_rates', methods=['GET'])
+def file_change_rates():
+    settings = get_settings()
+    project_dir = settings.get('project_dir', os.getcwd())
+    extensions = settings.get('extensions', None)
+    ignore_dir = settings.get('ignore_dir', None)
+    repo = ProjectDirectory(working_dir=project_dir)
+    cb = repo.file_change_rates(extensions=extensions, ignore_dir=ignore_dir, coverage=True)
+    cb.reset_index(level=0, inplace=True)
+    data = json.loads(cb.to_json(orient='records'))
+
+    output = {'data': []}
+    for blob in data:
+        row = [blob.get(x, None) for x in ['index', 'repository', 'unique_committers', 'net_rate_of_change', 'abs_rate_of_change', 'edit_rate', 'total_lines', 'coverage']]
+        row = [round(x, 2) if isinstance(x, float) else x for x in row]
+        output['data'].append(row)
+
+    return json.dumps(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
