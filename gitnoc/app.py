508b40cb259d4793e5b5c2437cbc1bef1085f91b
Will McGinnis <will@pedalwrencher.com>
2015-12-07 20:19:01 -0500

    added some basic config functionality and saving the plot data to disk

--- gitnoc/app.py
+++ gitnoc/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import json
+import os
 from gitpandas import ProjectDirectory
+from gitnoc.forms.public import SettingsForm
 
 TITLE = "GitNOC"
 
@@ -18,10 +20,15 @@ css = [
     "./css/main.css"
 ]
 
-# use list for explicit declaration or string with directory of repos for implicit
-project_dir = ['../../git-pandas', '../../stravalib']
-
 app = Flask(__name__)
+app.secret_key = 'CHANGEME'
+
+
+def get_settings():
+    try:
+        return json.load(open('settings.json', 'r'))
+    except:
+        return {}
 
 
 @app.route('/', methods=["GET"])
@@ -29,11 +36,48 @@ def index():
     return render_template('index.html', title=TITLE, base_scripts=scripts, css=css)
 
 
+@app.route('/settings', methods=["GET", "POST"])
+def settings():
+    form = SettingsForm(request.form)
+    if form.validate_on_submit():
+        settings = get_settings()
+        settings.update({
+            'ignore_dir': form.ignore_dir.data,
+            'project_dir': form.project_directory.data,
+            'extensions': form.extensions.data
+        })
+        json.dump(settings, open('settings.json', 'w'))
+
+    settings = get_settings()
+    extensions = settings.get('extensions', None)
+    if extensions is not None:
+        form.extensions.default = ', '.join(extensions)
+
+    project_dir = settings.get('project_dir', None)
+    if project_dir is not None:
+        if isinstance(project_dir, list):
+            form.project_directory.default = ', '.join(project_dir)
+        else:
+            form.project_directory.default = project_dir
+
+    ignore_dir = settings.get('ignore_dir', None)
+    if ignore_dir is not None:
+        form.ignore_dir.default = ', '.join(ignore_dir)
+
+    form.process()
+
+    return render_template('settings.html', form=form, title=TITLE, base_scripts=scripts, css=css)
+
+
 @app.route('/cumulative_author_blame', methods=['GET', 'POST'])
 def cumulative_author_blame():
-    # TODO: just write these to a static json file with a refresh button
+    settings = get_settings()
+    project_dir = settings.get('project_dir', os.getcwd())
+    extensions = settings.get('extensions', None)
+    ignore_dir = settings.get('ignore_dir', None)
+
     repo = ProjectDirectory(working_dir=project_dir)
-    cb = repo.cumulative_blame(extensions=['py'])
+    cb = repo.cumulative_blame(extensions=extensions, ignore_dir=ignore_dir)
     cb = cb[~cb.index.duplicated()]
     t = json.loads(cb.to_json(orient='columns'))
 
@@ -46,14 +90,22 @@ def cumulative_author_blame():
             blob['values'].append([int(float(data_point)), t[committer][data_point]])
             blob['values'] = sorted(blob['values'], key=lambda x: x[0])
         d3_data.append(blob)
-    return json.dumps(d3_data)
+
+    # dump the data to disk
+    filename = 'cumulative_author_blame.json'
+    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'))
+    return redirect(url_for('index'))
 
 
 @app.route('/cumulative_project_blame', methods=['GET', 'POST'])
 def cumulative_project_blame():
-    # TODO: just write these to a static json file with a refresh button
+    settings = get_settings()
+    project_dir = settings.get('project_dir', os.getcwd())
+    extensions = settings.get('extensions', None)
+    ignore_dir = settings.get('ignore_dir', None)
+
     repo = ProjectDirectory(working_dir=project_dir)
-    cb = repo.cumulative_blame(extensions=['py'], by='project')
+    cb = repo.cumulative_blame(extensions=extensions, ignore_dir=ignore_dir, by='project')
     cb = cb[~cb.index.duplicated()]
     t = json.loads(cb.to_json(orient='columns'))
 
@@ -66,7 +118,11 @@ def cumulative_project_blame():
             blob['values'].append([int(float(data_point)), t[committer][data_point]])
             blob['values'] = sorted(blob['values'], key=lambda x: x[0])
         d3_data.append(blob)
-    return json.dumps(d3_data)
+
+    # dump the data to disk
+    filename = 'cumulative_project_blame.json'
+    json.dump(d3_data, open(str(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'static' + os.sep + 'data' + os.sep + filename, 'w'))
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ gitnoc/forms/public.py
@@ -0,0 +1,35 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+__author__ = 'willmcginnis'
+
+
+class SettingsForm(Form):
+    project_directory = StringField('Project Directory (absolute path)', validators=[DataRequired()])
+    extensions = StringField('Extensions to Report On')
+    ignore_dir = StringField('Directories to Ignore')
+
+    def __init__(self, *args, **kwargs):
+        super(SettingsForm, self).__init__(*args, **kwargs)
+
+    def validate(self):
+        initial_validation = super(SettingsForm, self).validate()
+        if not initial_validation:
+            return False
+
+        self.project_directory.data = [str(x).strip() for x in self.project_directory.data.split(',')]
+        if len(self.project_directory.data) == 1:
+            self.project_directory.data = self.project_directory.data[0]
+
+        if self.extensions.data is not None:
+            self.extensions.data = [str(x).strip() for x in self.extensions.data.split(',')]
+            if len(self.extensions.data) == 1:
+                self.extensions.data = self.extensions.data[0]
+
+        if self.ignore_dir.data is not None:
+            self.ignore_dir.data = [str(x).strip() for x in self.ignore_dir.data.split(',')]
+            if len(self.ignore_dir.data) == 1:
+                self.ignore_dir.data = self.ignore_dir.data[0]
+
+        return True
